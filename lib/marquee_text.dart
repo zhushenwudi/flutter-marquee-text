0308f69f98a29cb908abbc86dadee161719615ab
zhushenwudi <30340027+zhushenwudi@users.noreply.github.com>
2022-08-11 15:27:16 +0800

    Update marquee_text.dart

--- lib/marquee_text.dart
+++ lib/marquee_text.dart
@@ -99,6 +99,18 @@ class _MarqueeContainerState extends State<_MarqueeContainer>
     _animationController.dispose();
     super.dispose();
   }
+  
+  static Size boundingTextSize(BuildContext context, String text, TextStyle style,  {int maxLines = 2^31, double maxWidth = double.infinity}) {
+    if (text == null || text.isEmpty) {
+      return Size.zero;
+    }
+    final TextPainter textPainter = TextPainter(
+        textDirection: TextDirection.ltr,
+        locale: Localizations.localeOf(context, nullOk: true),
+        text: TextSpan(text: text, style: style), maxLines: maxLines)
+      ..layout(maxWidth: maxWidth);
+    return textPainter.size;
+  }
 
   @override
   Widget build(BuildContext context) {
